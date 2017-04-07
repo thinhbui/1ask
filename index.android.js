fb07f0db06c195b97ddba49c9abfb85835704e50
thinhbui <buithinh2810@gmail.com>
2017-04-07 18:05:34 +0700

    fix text input

--- index.android.js
+++ index.android.js
@@ -4,7 +4,7 @@
  * @flow
  */
 // import './app/index'
-// import './App'
+import './App'
 // import './app/layouts/wait_question/notification'
 // import './app/layouts/lever_test/modal_test_lever'
 // import './app/layouts/wait_questzzion/index'
@@ -12,7 +12,7 @@
 // import './app/layouts/lever_test/nozztification_modal'
 // import './app/layzzouts/setting/'
 // import './testFirebase/App'
-import './app/layouts/chat/App'
+// import './app/layouts/chat/App'
 // import './app/layouts/login/login'
 // import './test'
 // import './firebase/App'
